c6bb2a3e67978f0d8c257e1b0e5366abb6476b86
Leonardo Amato <leo.amato@hotmail.com>
2022-10-14 11:53:38 +0200

    navigation added

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key key}) : super(key: key);
+  static const routeName = "/category-meals";
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routerArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryId = routerArgs["id"];
+    final categoryTitle = routerArgs["title"];
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("DeliMeals"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
